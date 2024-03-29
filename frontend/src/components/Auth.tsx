7a2d35ddb168f178ffe2cf0bea323dab04fe0716
Meetkumar Patel <meetkumar.patel@aircanada.ca>
2024-03-29 19:54:26 -0400

    pass "type" as props

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -51,10 +51,10 @@ interface LabelledInputType {
     type?: string;
 }
 
-function LabelledInput({ label, placeholder, onChange }: LabelledInputType) {
+function LabelledInput({ label, placeholder, onChange, type }: LabelledInputType) {
     return <div>
         <label className="block mb-2 text-sm font-medium text-black">{label}</label>
-        <input onChange={onChange} type={"type" || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
+        <input onChange={onChange} type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
 
     </div>
 
